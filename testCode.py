67695762ae468ad4928460452fbd6219e9de936e
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2021-07-29 01:05:43 +0530

    Made Map Upscaler

--- testCode.py
+++ testCode.py
@@ -57,3 +57,56 @@ mp=[[1,1., 1., 1., 1., 1., 1., 1., 1., 1, 1., 1., 1., 1, 1.,1],    #DEFAULT MAP
 ########################################################################################    
 
 
+
+########################################################################################
+######################################################################################## 
+##map upscler code
+
+map_Y_Miltiplier=int(miniMapResolution[0]/MapSize[0]) 
+map_X_Miltiplier=int(miniMapResolution[1]/MapSize[1])
+#these multiplier no whn multiplies to orignal size of the map gives map in almost 480p Res
+
+
+#this is where the higher resolution is calculated.
+mapy=MapSize[0]*map_Y_Miltiplier    #first using y multiplier 
+mapx=MapSize[1]*map_Y_Miltiplier
+if(mapx>miniMapResolution[1]):      #thn if the resolution is not corrrect 
+    mapy=MapSize[0]*map_X_Miltiplier#we will use x multipler 
+    mapx=MapSize[1]*map_X_Miltiplier
+    map_Y_Miltiplier=map_X_Miltiplier#the main multipler in all eq will be y multiplier so... put the right multiplier in it 
+    
+    
+
+
+
+#this is the map resolution close to 480p
+
+miniMap=np.zeros([mapy,mapx])
+miniMapInfo=np.zeros([mapy,mapx])
+i=0
+y=0
+mmi=0
+mmy=0
+while True:
+    if(mmi<MapSize[0]):
+        y=0
+        mmy=0
+        while True:
+            if(mmy<MapSize[1]):
+                #print(mmi)
+                #print(mmy)
+                if(mp[mmi][mmy]==1):
+                    for x in range(i,i+map_Y_Miltiplier):
+                        for z in range(y,y+map_Y_Miltiplier):
+                            miniMap[x][z]=1
+                            miniMapInfo[x][z]=1
+                y+=map_Y_Miltiplier
+                mmy+=1
+            else:
+                break
+    else:
+        break
+    i+=map_Y_Miltiplier
+    mmi+=1
+########################################################################################
+######################################################################################## 
