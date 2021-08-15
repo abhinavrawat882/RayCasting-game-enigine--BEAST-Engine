743e8de93fd77168c3873689a3744939d4e7b867
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2021-08-15 15:28:26 +0530

    Update LineTracer.py
    Code Outline for Line tracer

--- LineTracer.py
+++ LineTracer.py
@@ -1,1 +1,20 @@
-
+import math
+def lineTracer(y,x,LineAngle,levelMap):
+  lineTangent=math.tan(angle)
+  
+  delx
+  dely
+  
+  lineTraceStat=0
+  
+  while lineTraceStat==0:
+    
+    isYInterceptSucess=0
+    
+    
+    ##Calculate Y intercept 
+    
+    
+    ## CAlculate x if y not found
+    
+    
