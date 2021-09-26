ea81dc9c318b18cc594dfa3b76f752e8725a38ac
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2021-09-26 21:56:14 +0530

    new improved acurate line tracer

--- LineTracer.py
+++ LineTracer.py
@@ -1,64 +1,141 @@
 import math
-def lineTracer(y,x,LineAngle,levelMap):
-  lineTangent=math.tan(angle)
-  
-  delx=x-int(x)
-  dely=y-int(y)
-  
-  lineTraceStat=0
-  
-  while lineTraceStat==0:
+def lineTracer(y,x,lineAngle,Levelmap):##3.0
     
-     foundYIntercept=0
-        #if y intercept is not found then x axis is calculated
+    
+    lineLen=0
+    #length of line is stored in this variable
+    
+    ##first thing find slope of the line
+    m=math.tan(lineAngle*rd)
+
+    ##now unit distance for this line in any direction will be 
+    
+    
+    
+    Sx=(1 + m**2)**(0.5)
+    Sy=(1 + 1/(m**2))**(0.5)
+
+    
+    
+    
+    ## now lets find out the direction for the line ...
+    muly=1
+    mulx=1
+    if (lineAngle<180 and lineAngle>90):
         
+        mulx=-1
+    elif (lineAngle>180 and lineAngle<270):
+        mulx=-1
+        muly=-1
+    elif (lineAngle<360 and lineAngle>270):
+        muly=-1
+    
+    
+    
+    
+    
+    # therefore  lets get length of x and y ... that will be added rto get final quardinates 
+    
+    ylen=0
+    
+    xlen=0
+
+    ##now calculating deviation from the edge delx and dely
+    dely=1
+    delx=1
+
+    ### If angle of line is in range 90 and 0 
+    
+    if lineAngle>0 and lineAngle<90:
         
-        ##############       Y AXIS            ################
-        if lineAngle<90 or lineAngle>270:
-            xa=1-delx
-            h=lineTangent(xa)
-            
-        ##code working ok sudo checked
+        dely=y-int(y)
+        delx=1-(x-int(x))
+    
+    ### If angle of line is in range 90 an
+    if lineAngle>90 and lineAngle<180:
+        dely=y-int(y)
+        delx=(x-int(x))
+    
+
+
+    ### If angle of line is in range 180 and 270  
+    if lineAngle>180 and lineAngle<270:
+        dely=1-(y-int(y))
+        delx=(x-int(x))
+
+    ### If angle of line is in range 270 and 360 
+    if lineAngle>270 and lineAngle<360:
+        dely=1-(y-int(y))
+        delx=1-(x-int(x))
+    print(dely,delx)
+        
+    # now calculating the first intersepts
+    lx=Sx*delx
+    ly=Sy*dely
+    
+    if(lx<ly):
+        lineLen=lx
+        leny=ly-lx
+        lenx=0
+    else:
+        lineLen=ly
+        lenx=lx-ly
+        leny=0
+    print("SX",Sx)
+    print("SY",Sy)
+    print("DX",delx)
+    print("DY",dely)
+    print("LX",lx)
+    print("LY",ly)
+    print("lineLen",lineLen)
+    tmpdelx=x+(mulx*delx)
+    tmpdely=y+(muly*dely)
+    if x==-1:
+        tmpdelx-=1
+    if y==-1:
+        tmpdely-=1
+    if(Levelmap[int(tmpdely)][int(tmpdelx)]!=0):
+        return(lineLen)
+        
+    
+    while True: ##main loop
+        ##lenfor eaxh axis
+        
+        if lenx==0:
+            lenx=Sx
+        if leny==0:
+            leny=Sy
+        
+        #comapring length 
+        if lenx<leny:
+            leny-=lenx
+            lineLen+=lenx
+            lenx=0
+            delx+=1
+            tmpdelx+=1
             
         else:
-            if(delx!=0):
-                xa=-delx
-                h=lineTangent(xa)
-                
-            else:
-                xa=-1
-                h=lineTangent(xa)
+            lenx-=leny
+            lineLen+=leny
+            leny=0
+            dely+=1
+            tmpdely+=1
+        ### Checking if a wall or any thing is found in the locaiton reached
         
-        if (dely-h>=0 and dely-h<=1) or (dely==0 and h<=-1 and h<0):
-            x+=xa
-            y-=h
-            dely-=h
-            delx=0
-            foundYIntercept=1
+        #print("SX",Sx)
+        #print("SY",Sy)
+        print("DX",delx)
+        print("DY",dely)
+        print("LX",lx)
+        print("LY",ly)
+        print("lineLen",lineLen)
         
-        
-        ##############       X AXIS            ################
-        if(foundYIntercept==0):#if y intercept failed find x intercept
-            
-            if lineAngle>0 or lineAngle<180:
-                if dely!=0:
-                    b=dely/lineTangent
-                else:
-                    b=1/lineTangent
-            else:
-                b=(1-del)/lineTangent
-            
-            if delx+b==1 or delx+b==0:
-                dely=0
-                delx=
-                
-        
-    
-      
-      
-      
-      
-    
-    ## CAlculate x if y not found
-    
-    
+        if x==-1:
+            tmpdelx-=1
+        if y==-1:
+            tmpdely-=1
+        print(tmpdely,tmpdelx)
+        print(Levelmap[int(tmpdely)][int(tmpdelx)])
+        if(Levelmap[int(tmpdely)][int(tmpdelx)]!=0):
+            break
+    return(lineLen)
